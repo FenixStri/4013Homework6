26389d6284911e4edef83d5b62b1c06a4d6f7842
Fenix <89755740+FenixStri@users.noreply.github.com>
2022-11-16 11:49:21 -0600

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="library1.php">Java Script 1 Page</a>
+          <a class="nav-link" href="library1.php">Java Script Library 1 Page</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="javascript2.php">Java Script 2 Page</a>
